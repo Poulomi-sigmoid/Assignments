ddcde01813a6183935df23bdacc0580e1d86b5c9
Poulomi Chandra <poulomic@sigmoidanalytics.com>
2022-02-08 22:52:55 +0530

    Added MongoDB Assignment

---
+++ MongoDBAssignment/q4b_iv.py
@@ -0,0 +1,85 @@
+from pymongo import MongoClient
+
+def task():
+    n = int(input("Enter a number : "))
+    pipeline = [
+        {
+            '$unwind': {
+                'path': '$genres',
+                'preserveNullAndEmptyArrays': False
+            }
+        }, {
+            '$project': {
+                'title': 1,
+                'rating': '$imdb.rating',
+                'genres': 1
+            }
+        }, {
+            '$group': {
+                '_id': '$genres',
+                'movies': {
+                    '$push': {
+                        'movie': '$title',
+                        'rating': '$rating'
+                    }
+                }
+            }
+        }, {
+            '$unwind': {
+                'path': '$movies',
+                'preserveNullAndEmptyArrays': False
+            }
+        }, {
+            '$sort': {
+                'movies.rating': -1
+            }
+        }, {
+            '$match': {
+                'movies.rating': {
+                    '$ne': ''
+                }
+            }
+        }, {
+            '$group': {
+                '_id': '$_id',
+                'movies': {
+                    '$push': '$movies'
+                }
+            }
+        }, {
+            '$project': {
+                'movies': {
+                    '$slice': [
+                        '$movies', 0, n
+                    ]
+                }
+            }
+        }
+    ]
+
+    results = collection.aggregate(pipeline)
+    for result in results:
+        print()
+        print(f"Genre : {result['_id']}")
+        print("------------------------")
+        for i in range(len(result['movies'])):
+            print(f"Movie : {result['movies'][i]['movie']} ;  Rating : {result['movies'][i]['rating']}")
+        print()
+        print("========================")
+
+
+if __name__ == "__main__":
+
+    try:
+        client = MongoClient("mongodb://127.0.0.1:27017")
+        print("Connection successful")
+        print(client.server_info())
+
+        collection = client['moviesData']['movies']
+        print("Q4) Create Python methods and MongoDB queries to support the below operations -")
+        print('''Choices : 
+        Find top `N` movies for each genre with the highest IMDB rating.
+            ''')
+        task()
+    except:
+        print("Could not establish connection")
